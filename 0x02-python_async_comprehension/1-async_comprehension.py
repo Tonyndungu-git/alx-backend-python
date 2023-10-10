6788b9a8c572d47a653a6dc92fa775d61d651f58
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-10-10 22:30:02 +0300

    async comprehension correction

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,8 +2,13 @@
 ''' async comprehension'''
 
 from typing import List
+from importlib import import_module as using
+
+
 
 async_generator = __import__('0-async_generator').async_generator
+
+
 
 async def async_comprehension() -> List[float]:
     ''' async comprehension '''
